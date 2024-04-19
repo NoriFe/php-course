d95a60c8471013d513cf95bdf7191034016b4b9e
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-04-19 12:10:34 +0100

    learning arithmetic operators and math functions in php

--- index.php
+++ index.php
@@ -1,24 +1,97 @@
 <?php
-$title = "Introduction to PHP";
-$author = "John Doe";
-$body = "PHP (Hypertext Preprocessor) is a widely used server-side scripting language that has revolutionized web development. With its simplicity, flexibility, and vast community support, PHP has become the backbone of countless dynamic websites and web applications.";
-$pageTitle = 'Brad\'s PHP Blog | ' . $title;
+/*
+| Arithmetic Operators
+| Operator | Description    |
+| -------- | -------------- |
+| `+`      | Addition       |
+| `-`      | Subtraction    |
+| `*`      | Multiplication |
+| `/`      | Division       |
+| `%`      | Modulus        |
+*/
+$output = null;
+
+$num1 = 20;
+$num2 = 10;
+// basic math operations
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 - $num2 = " . $num1 - $num2;
+$output = "$num1 * $num2 = " . $num1 * $num2;
+$output = "$num1 / $num2 = " . $num1 / $num2;
+$output = "$num1 % $num2 = " . $num1 % $num2;
+// assignment operators
+
+$num3 = 10;
+// $num3 = $num3 + 10;
+$num3 /= 2;
+
+
+// build-in php functions
+
+//random number
+
+$output = rand(1, 10);
+
+//round number
+
+$output = round(2.4);
+
+// ceil number
+
+$output = ceil(2.4);
+
+//floor number
+
+$output = floor(2.6);
+
+// "square" sqrt number
+
+$output = sqrt(16);
+
+// pi number
+
+$output = pi();
+
+// "absolute" abs number
+
+$output = abs(-100);
+
+// max number
+
+$output = max(1, 2, 3, 4, 5);
+
+// min number
+
+$output = min(1, 2, 3, 4, 5);
+
+$output = number_format(1234567.199312, 2, '.', ',');
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?=  $pageTitle ?></title>
-  </head>
-  <body>
-    <main>
-      <h1><?= $title ?></h1>
-      <p>By: <?= $author ?></p>
-      <p>
-        <?= $body ?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <!-- Output -->
+      <p class="text-xl">
+        <?= $output ?>
       </p>
-    </main>
-  </body>
+    </div>
+  </div>
+</body>
+
 </html>
