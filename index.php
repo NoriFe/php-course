6166a834dc530f3f40d35513e932b3c7c70b2f25
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-04-26 13:32:49 +0100

    learning about string functions in php

--- index.php
+++ index.php
@@ -1,73 +1,17 @@
 <?php
-/*
-| Arithmetic Operators
-| Operator | Description    |
-| -------- | -------------- |
-| `+`      | Addition       |
-| `-`      | Subtraction    |
-| `*`      | Multiplication |
-| `/`      | Division       |
-| `%`      | Modulus        |
-*/
 $output = null;
+$string = 'hello You';
 
-$num1 = 20;
-$num2 = 10;
-// basic math operations
-$output = "$num1 + $num2 = " . $num1 + $num2;
-$output = "$num1 - $num2 = " . $num1 - $num2;
-$output = "$num1 * $num2 = " . $num1 * $num2;
-$output = "$num1 / $num2 = " . $num1 / $num2;
-$output = "$num1 % $num2 = " . $num1 % $num2;
-// assignment operators
-
-$num3 = 10;
-// $num3 = $num3 + 10;
-$num3 /= 2;
-
-
-// build-in php functions
-
-//random number
-
-$output = rand(1, 10);
-
-//round number
-
-$output = round(2.4);
-
-// ceil number
-
-$output = ceil(2.4);
-
-//floor number
-
-$output = floor(2.6);
-
-// "square" sqrt number
-
-$output = sqrt(16);
-
-// pi number
-
-$output = pi();
-
-// "absolute" abs number
-
-$output = abs(-100);
-
-// max number
-
-$output = max(1, 2, 3, 4, 5);
-
-// min number
-
-$output = min(1, 2, 3, 4, 5);
-
-$output = number_format(1234567.199312, 2, '.', ',');
+$output = strlen($string); // counts the number of characters in a string
+$output = str_word_count($string); // counts the number of words in a string  
+$output = strpos($string, 'You'); // finds the position of the first occurrence of a substring in a string
+$output = str_replace('You', 'World', $string); // replaces some characters with some other characters in a string
+$output = strtoupper($string); // converts a string to uppercase
+$output = strtolower($string); // converts a string to lowercase
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -86,10 +30,7 @@ $output = number_format(1234567.199312, 2, '.', ',');
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
-      <p class="text-xl">
-        <?= $output ?>
-      </p>
+      <p class="text-xl"><?= $output ?></p>
     </div>
   </div>
 </body>
