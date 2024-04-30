3a589fa0b77145917dfab9d8ff1f37439627aa25
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-04-30 17:15:20 +0100

    leraning dates and time in php

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 $output = null;
-$string = 'hello You';
-
-$output = strlen($string); // counts the number of characters in a string
-$output = str_word_count($string); // counts the number of words in a string  
-$output = strpos($string, 'You'); // finds the position of the first occurrence of a substring in a string
-$output = str_replace('You', 'World', $string); // replaces some characters with some other characters in a string
-$output = strtoupper($string); // converts a string to uppercase
-$output = strtolower($string); // converts a string to lowercase
-
-
+$output = date('Y-m-d H:i:s'); // get current date and time
+$output = date('Y', 936345600); // get year from timestamp
+$output = date('Y-m-d H:i:s', strtotime('2020-12-31 23:59:59')); // convert string to timestamp
+$output = date('Y-m-d H:i:s', strtotime('tomorrow')); // convert string to timestamp
+$output = date('Y-m-d H:i:s', strtotime('next Monday')); // convert string to timestamp
+$output = date('m'); // get month
+$output = date('d'); // get day
+$output = date('Y'); // get year
+$output = date('l'); // get day of the week
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>PHP From Scratch</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
 </head>
 
 <body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <!-- Output -->
+            <p class="text-xl"><?php echo $output; ?></p>
+        </div>
     </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <p class="text-xl"><?= $output ?></p>
-    </div>
-  </div>
 </body>
 
 </html>
