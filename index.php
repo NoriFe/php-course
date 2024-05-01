d2575cf3e93139e5a362b79696164757bad4ed79
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-01 17:44:19 +0100

    small intro to arrays in php

--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <?php
-$output = null;
-$output = date('Y-m-d H:i:s'); // get current date and time
-$output = date('Y', 936345600); // get year from timestamp
-$output = date('Y-m-d H:i:s', strtotime('2020-12-31 23:59:59')); // convert string to timestamp
-$output = date('Y-m-d H:i:s', strtotime('tomorrow')); // convert string to timestamp
-$output = date('Y-m-d H:i:s', strtotime('next Monday')); // convert string to timestamp
-$output = date('m'); // get month
-$output = date('d'); // get day
-$output = date('Y'); // get year
-$output = date('l'); // get day of the week
+
+$names = array("John", "Jane", "Joe", "Jill");
+$numbers = [1, 2, 3, 4, 5]; 
+
+
+
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';  // <pre> is a tag in HTML that defines preformatted text. Text in a <pre> element is displayed in a fixed-width font, and the text preserves both spaces and line breaks.
+
+}
+
+// inspect($numbers); //calling the function inspect and passing the array $numbres as an argument
+
+// print_r($names); //print_r — Prints human-readable information about a variable
+
+// echo $names[1]; //prints Jane
+
+// echo $numbers[3]; //prints 4
+
+// echo $names[4]; //prints error because the index 4 does not exist in the array $names
+// adding an element to an array
+
+$numbers[] = 100; //adds 100 to the end of the array
+
+$numbers[3] = 13333; //changes the value of the element at index 3 to 13333
+
+inspect($numbers); //prints array(2) { [0]=> int(100) [1]=> int(200) }
+
+
+
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>PHP From Scratch</title>
-</head>
-
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
-        </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-            <!-- Output -->
-            <p class="text-xl"><?php echo $output; ?></p>
-        </div>
-    </div>
-</body>
-
-</html>
