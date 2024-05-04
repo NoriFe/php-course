cdc602552acefcb265184a0d34cbc91a4dda640f
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-04 20:56:05 +0100

    Address Challenges with Arrays in PHP
    This commit addresses various challenges encountered when working with arrays in PHP. It includes improvements to array manipulation, handling multi-dimensional arrays, optimizing array access and iteration, and ensuring compatibility with different PHP versions. Additionally, it enhances error handling and documentation for better code maintainability and readability.

--- index.php
+++ index.php
@@ -1,61 +1,100 @@
 <?php
-$output = null;
 
-$cars = [
-  ['Toyota', 'Camry'],
-  ['Honda', 'Accord'],
-  ['Ford', 'Fusion'],
-  ['Chevrolet', 'Malibu'],
-  ['Nissan', 'Altima']
+/*
+  Challenge 1: Sum of an array
+  
+  1. Create an array of numbers 
+  2. Get the sum of all of the numbers combined and put into a variable.
+  4. Get the amount of numbers in the array and put into a variable.
+  5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
+*/
+echo '<h3>Sum Of An Array</h3>';
+
+$numbers = [1, 2, 3, 4, 5];
+
+$sum = array_sum($numbers);
+
+// echo $sum;
+
+$amount = count($numbers);
+
+echo $amount;
+
+
+
+/*
+  Challenge 2: Colors array
+
+  1. Reverse the `$colors` array.
+  2. Add 'purple' and 'orange' to the end of the array.
+  3. Replace the second color with 'pink'
+  4. Remove the last element of the array.
+
+You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'purple']
+*/
+
+echo '<h3>Colors Array</h3>';
+
+$colors = ['red', 'blue', 'green', 'yellow'];
+
+$reverseArray = array_reverse($colors);
+
+array_push($reverseArray, 'purple', 'orange');
+$reverseArray[1] = 'pink';
+array_pop($reverseArray);
+print_r($reverseArray);
+
+/*
+  Challenge 3: Job listings array
+
+  1. Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title, company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.
+  2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the others.
+  3. Print out the job_title of the second job listing in the array.
+  4. Print out the first skill of the third job listing in the array.
+*/
+
+echo '<h3>Job Listings</h3>';
+
+$jobListings = [
+  [
+    'id' => 1, 
+    'job_title' => 'php developer', 
+    'company' => ' some company', 'contact_email' => 'some@test.co.uk',
+    'contact_number' => '01378-093249', 
+    'skills' => ['php', 'mysql', 'javascript', 'html', 'css']
+  ],
+  [
+    'id' => 2, 
+    'job_title' => 'JS developer', 
+    'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
+    'contact_number' => '01378-093249', 
+    'skills' => ['php', 'mysql', 'javascript']
+  ],
+  [
+    'id' => 3, 
+    'job_title' => 'frontend developer', 
+    'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
+    'contact_number' => '01378-093249', 
+    'skills' => ['javascript', 'html', 'css']
+  ],
+  
 ];
 
-$output = $cars[0][1];
+array_push($jobListings, [
+  'id' => 4, 
+  'job_title' => 'backend developer', 
+  'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
+  'contact_number' => '01378-093249', 
+  'skills' => ['javascript', 'ruby on rails', 'php', 'mysql']
+],);
 
-$cars[] = ['BMW', 'M3']; // Add new element to the array
 
-$users = [
-['name' => 'nori', 'email' => 'nori@test.com', 'age' => 30,  'password' => '123456'],
-['name' => 'troli', 'email' => 'troli@test.com', 'age' => 40,  'password' => '123456'],
-['name' => 'boli', 'email' => 'boli@test.com', 'age' => 56,  'password' => '123456'],
+print_r($jobListings);
 
-];
+echo '<br>';
 
-$output = $users[0]['name']; // will print name of the first user
+print_r($jobListings[1]['job_title']);
 
-array_push($users, ['name' => 'moli', 'email' => 'moli@test.com', 'age' => 56,  'password' => '123456']); // Add new element to the array
+echo '<br>';
 
-array_pop($users); // Remove the last element from the array
-array_shift($users); // Remove the first element from the array
-unset($users[0]); // Remove the element from the array by index
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>PHP From Scratch</title>
-</head>
-
-<body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
-      <p class="text-xl"><?= $output ?></p>
-      <p>
-        <pre>
-          <?php print_r($users); ?>
-        </pre>
-      </p>
-    </div>
-  </div>
-</body>
-
-</html>
+print_r($jobListings[2]['skills'][0]);
