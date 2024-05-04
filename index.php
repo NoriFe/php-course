ddda134b7df5fb4fc21f38a012193c0619e14ba3
norbert skwierczynski <norbert.fe.dev@gmail.com>
2024-05-04 19:28:37 +0100

    learning how to associative arrays in php

--- index.php
+++ index.php
@@ -1,60 +1,21 @@
 <?php
 $output = null;
 
-$ids = [11, 23, 33, 42, 53, 641, 72, 84, 95, 101];
-$users = [ 'user2', 'user1', 'user3', 'user5', 'user4'];
+$user = [
+    'name' => 'Norbert',
+    'email' => 'norbert@test.com',
+    'password' => '123456',
+    'hobbies' => [
+        'Reading',
+        'Coding',
+        'Gaming'
+    ]
+    ];
+    $output = $user['name'];
+    $output .= '<br>';
+    $output .= $user['email'];
 
-$output = count($ids) === count($users) ? 'The arrays have the same length' : 'The arrays do not have the same length';
-
-// sort
-sort($ids);
-sort($users);
-
-//push
-
-array_push($ids, 111);
-array_push($users, 'user6');
-
-//pop removing last element
-
-array_pop($ids);
-array_pop($users);
-
-//array_shift removing first element
-
-array_shift($ids);
-array_shift($users);
-
-//array_unshift adding element at the beginning
-
-array_unshift($ids, 1);
-array_unshift($users, 'user0');
-
-//slice array 
-
-$ids = array_slice($ids, 0, 5); // 5 is the length of the array
-
-// splice array
-
-array_splice($users, 0, 1, 'new ID'); // 0 is the starting index, 1 is the number of elements to remove, 'new ID' is the element to add
-
-// sum of all elements in the array
-
-$output = 'sum of ids: ' . array_sum($ids);
-
-// explode 
-
-$tags = 'php,javascript,css,html';
-$tagsArray = explode(',', $tags);
-
-//var_dump($tagsArray);
-
-//implode 
-
-$output = implode(' - ', $tagsArray);
-
-
-
+    $user['address'] = 'perth street'; // adding new key value pair
 ?>
 
 <!DOCTYPE html>
@@ -77,18 +38,11 @@ $output = implode(' - ', $tagsArray);
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
-            <h2 class="text-xl font-semibold my-4">IDs array:</h2>
+            <h2 class="text-xl font-semibold my-4">User Array: </h2>
             <p>
-                <pre>
-                    <?php print_r($ids); ?>
-                </pre>
-            </p>
-            <h2 class="text-xl font-semibold my-4">Users array:</h2>
-            <p>
-                <pre>
-                    <?php print_r($users); ?>
-                </pre>
-            </p>
+              <pre>
+                <?php var_dump($user); ?>
+              </pre>
             </p>
         </div>
     </div>
