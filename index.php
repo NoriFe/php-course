ded3d86ef158d671a54633f3c7cc3c42f55e3bf1
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-05 12:36:23 +0100

    learning nested loops in php

--- index.php
+++ index.php
@@ -1,55 +1,60 @@
 <?php
+  // for ($i = 0; $i <= 5; $i++) {
+  //   for ($j = 1; $j <= 5; $j++) {
+  //     echo $i .' - ' . $j . '<br>';
+  //   }
+  // }
 
-   // basic for loop
-   // for($i = 0; $i < 10; $i++) {
-   //    echo $i . "<br>";        
-   //}
+  // $i =0;
 
-   // basic while loop
+  // while ($i <= 5) {
+  //   $j = 0;
+  //   while ($j <= 5) {
+  //     echo $i .' - ' . $j . '<br>';
+  //     $j++;
+  //   }
+  //   $i++;
+  // }
 
-    // $i = 0;
 
-    // while($i < 10) {
-    //     echo $i . "<br>";
-    //     $i++;
-    // }
-
-    // do while loop
-
-    // $i = 0;
-
-    // do {
-    //     echo $i . "<br>";
-    //     $i++;
-    // } while($i < 10);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>PHP From Scratch</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nested Loops & CSS Grid Example</title>
+  <style>
+    .grid-container {
+      display: grid;
+      grid-template-columns: repeat(5, 50px);
+      /* Create 5 columns, each 50px wide */
+      grid-gap: 5px;
+      /* Add some spacing between grid items */
+    }
+
+    .grid-item {
+      width: 50px;
+      height: 50px;
+      background-color: lightblue;
+      text-align: center;
+      line-height: 50px;
+    }
+  </style>
 </head>
 
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+<body>
+  <div class="grid-container">
+    <?php for ($i = 0; $i < 5; $i++) : ?>
+      <?php for ($j = 1; $j < 5; $j++) : ?>
+        <div class="grid-item">
+          <?=  $i . ' - ' . $j; ?>
         </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-            <!-- Output -->
-            <ul>
-                <?php $i =0; do { ?>
-                <li><?php echo $i; ?></li>
-                <?php $i++; } while($i < 10); ?>
-            </ul>
-        </div>
-    </div>
+      <?php endfor ?>
+    <?php endfor ?>
+  </div>
 </body>
 
 </html>
