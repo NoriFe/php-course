90c1b6e2d24196a20cef22635af6e0211c97f887
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-05 12:22:41 +0100

    learning loops in php

--- index.php
+++ index.php
@@ -1,100 +1,55 @@
 <?php
 
-/*
-  Challenge 1: Sum of an array
-  
-  1. Create an array of numbers 
-  2. Get the sum of all of the numbers combined and put into a variable.
-  4. Get the amount of numbers in the array and put into a variable.
-  5. Print out 'The sum of the {amount} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'. 
-*/
-echo '<h3>Sum Of An Array</h3>';
+   // basic for loop
+   // for($i = 0; $i < 10; $i++) {
+   //    echo $i . "<br>";        
+   //}
 
-$numbers = [1, 2, 3, 4, 5];
+   // basic while loop
 
-$sum = array_sum($numbers);
+    // $i = 0;
 
-// echo $sum;
+    // while($i < 10) {
+    //     echo $i . "<br>";
+    //     $i++;
+    // }
 
-$amount = count($numbers);
+    // do while loop
 
-echo $amount;
+    // $i = 0;
 
+    // do {
+    //     echo $i . "<br>";
+    //     $i++;
+    // } while($i < 10);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-/*
-  Challenge 2: Colors array
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
+</head>
 
-  1. Reverse the `$colors` array.
-  2. Add 'purple' and 'orange' to the end of the array.
-  3. Replace the second color with 'pink'
-  4. Remove the last element of the array.
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <!-- Output -->
+            <ul>
+                <?php $i =0; do { ?>
+                <li><?php echo $i; ?></li>
+                <?php $i++; } while($i < 10); ?>
+            </ul>
+        </div>
+    </div>
+</body>
 
-You should end up with the following array: ['yellow', 'pink', 'blue', 'red', 'purple']
-*/
-
-echo '<h3>Colors Array</h3>';
-
-$colors = ['red', 'blue', 'green', 'yellow'];
-
-$reverseArray = array_reverse($colors);
-
-array_push($reverseArray, 'purple', 'orange');
-$reverseArray[1] = 'pink';
-array_pop($reverseArray);
-print_r($reverseArray);
-
-/*
-  Challenge 3: Job listings array
-
-  1. Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title, company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.
-  2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the others.
-  3. Print out the job_title of the second job listing in the array.
-  4. Print out the first skill of the third job listing in the array.
-*/
-
-echo '<h3>Job Listings</h3>';
-
-$jobListings = [
-  [
-    'id' => 1, 
-    'job_title' => 'php developer', 
-    'company' => ' some company', 'contact_email' => 'some@test.co.uk',
-    'contact_number' => '01378-093249', 
-    'skills' => ['php', 'mysql', 'javascript', 'html', 'css']
-  ],
-  [
-    'id' => 2, 
-    'job_title' => 'JS developer', 
-    'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
-    'contact_number' => '01378-093249', 
-    'skills' => ['php', 'mysql', 'javascript']
-  ],
-  [
-    'id' => 3, 
-    'job_title' => 'frontend developer', 
-    'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
-    'contact_number' => '01378-093249', 
-    'skills' => ['javascript', 'html', 'css']
-  ],
-  
-];
-
-array_push($jobListings, [
-  'id' => 4, 
-  'job_title' => 'backend developer', 
-  'company' => ' some company', 'contact_email' => 'some@test.co.uk', 
-  'contact_number' => '01378-093249', 
-  'skills' => ['javascript', 'ruby on rails', 'php', 'mysql']
-],);
-
-
-print_r($jobListings);
-
-echo '<br>';
-
-print_r($jobListings[1]['job_title']);
-
-echo '<br>';
-
-print_r($jobListings[2]['skills'][0]);
+</html>
